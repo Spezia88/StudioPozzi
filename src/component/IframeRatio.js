e684d4d8d1c431b53d1ddc5b16a12dcaad229b29
Spezia88 <cristian.speziale@gmail.com>
2021-05-02 20:48:28 +0200

    Fix height iframe ratio

--- src/component/IframeRatio.js
+++ src/component/IframeRatio.js
@@ -12,10 +12,10 @@ const IframeRatio =()=>{
 		
 		 <Iframe url="https://box.ratio.it/2/index.php?bg=ffffff&text=333333&link=45bdbf&news=428bca&border=dddddd&logo=logo6&t=959725b570d3d0d51e18e799a987a8c0"
 				        width="100%"
-				        height="450px"
+				        height="100px"
 				        display="initial"
 				        position="relative"
-				        styles={{border:"1px solid #d9d9d9", overflow:"visible", width:"100%", height:"100px"}}
+				        styles={{border:"1px solid #d9d9d9", overflow:"visible", width:"100%"}}
 				        allowFullScreen />
 		
 	)
